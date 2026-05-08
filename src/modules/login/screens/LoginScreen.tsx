715161ee9136eab764cb150e85922c37f87540c6
Jeferson Figueiredo <jefersonlrf@gmail.com>
2026-05-08 06:30:27 -0300

    feat:create module login

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import { BackgroundImage } from "../styles/LoginScreen.styles";
+
+const LoginScreen = () => {
+
+    return (<div>
+        <BackgroundImage src="./background.png" />
+    </div>
+    )
+};
+
+export default LoginScreen;
